f3c4152e65464470a4e967a05372450ce4944380
Joao Micena (JP) <joao.micena@nubank.com.br>
2020-08-16 21:08:31 -0300

    Refactor account controller to use the new print-result fn

--- src/personal_finances/controllers/account.clj
+++ src/personal_finances/controllers/account.clj
@@ -3,6 +3,7 @@
             [clojure.spec.alpha                  :as s]
             [personal-finances.format            :as fmt]
             [personal-finances.logic.coercion    :as l-coe]
+            [personal-finances.logic.utils       :as l-utl]
             [personal-finances.models.account    :as m-acc]))
 
 (defn- add-account
@@ -12,11 +13,9 @@
        db-conn     (db)
        acc         {:category lc-category :name lc-name}]
    (if (some #{lc-category} valid-categories)
-     (try {:success
-           (str
-             "Account " lc-name " on category " lc-category " added. id: " (-> acc
-                                                                               (m-acc/insert-account! db-conn)
-                                                                               (get (keyword "last_insert_rowid()"))))}
+     (try {:success (-> acc
+                        (m-acc/insert-account! db-conn)
+                        (get (keyword "last_insert_rowid()")))}
           (catch Exception e {:failure (ex-message e)}))
      {:failure (str "Invalid category. Valid categories: " (string/join ", " valid-categories))})))
 
@@ -24,15 +23,13 @@
   [db]
   (let [db-conn (db)]
     (try
-      {:success (-> db-conn m-acc/read-accounts fmt/table)}
+      {:success (m-acc/read-accounts db-conn)}
       (catch Exception e {:failure (ex-message e)}))))
 
 (defn- remove-account
   [id db]
   (let [db-conn (db)]
-    (try {:success
-          (str "Number of accounts deleted: "
-               (-> (m-acc/delete-account! id db-conn) :next.jdbc/update-count))}
+    (try {:success (-> (m-acc/delete-account! id db-conn) :next.jdbc/update-count)}
          (catch Exception e {:failure (ex-message e)}))))
 
 (defn get-account
@@ -46,17 +43,18 @@
 (defn- account-add-cmd
   [valid-categories {:keys [category name system]}]
   (let [result (add-account category name valid-categories (:database system))]
-    (-> result first val println)))
+    (l-utl/print-result #(str "Account added [id: " % "]") result)))
 
 (defn- account-list-cmd
   [{:keys [system]}]
   (let [result (list-accounts (:database system))]
-    (-> result first val println)))
+    (l-utl/print-result fmt/table result)))
 
 (defn- account-remove-cmd
   [{:keys [id system]}]
-  (let [coerced-id (l-coe/int-like id)]
-    (-> coerced-id (remove-account (:database system)) first val println)))
+  (let [coerced-id (l-coe/int-like id)
+        result (remove-account coerced-id (:database system))]
+    (l-utl/print-result #(str "Account deleted [count: " % "]") result)))
 
 (def account-add-handler
   #:personal-finances.cmd{:name ["account" "add"]
